8e47f285ce58380a37f93e63cbf1cb3ae4313aed
Michael N. Lipp <mnl@mnl.de>
2022-05-17 23:20:20 +0200

    Add experimental preferences icon.

---
+++ de.mnl.moodle.webconlet.prefs/src/de/mnl/moodle/webconlet/prefs/MoodlePrefsConlet.java
@@ -0,0 +1,105 @@
+/*
+ * Moodle Tools Console
+ * Copyright (C) 2022 Michael N. Lipp
+ *
+ * This program is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 3 of the License, or 
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.mnl.moodle.webconlet.prefs;
+
+import freemarker.core.ParseException;
+import freemarker.template.MalformedTemplateNameException;
+import freemarker.template.Template;
+import freemarker.template.TemplateNotFoundException;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import org.jgrapes.core.Channel;
+import org.jgrapes.core.Event;
+import org.jgrapes.core.Manager;
+import org.jgrapes.core.annotation.Handler;
+import org.jgrapes.webconsole.base.Conlet.RenderMode;
+import org.jgrapes.webconsole.base.ConsoleSession;
+import org.jgrapes.webconsole.base.events.AddConletType;
+import org.jgrapes.webconsole.base.events.AddPageResources.ScriptResource;
+import org.jgrapes.webconsole.base.events.ConsoleReady;
+import org.jgrapes.webconsole.base.events.RenderConlet;
+import org.jgrapes.webconsole.base.events.RenderConletRequestBase;
+import org.jgrapes.webconsole.base.freemarker.FreeMarkerConlet;
+
+/**
+ * Example of a simple conlet.
+ */
+public class MoodlePrefsConlet extends FreeMarkerConlet<Serializable> {
+
+    private static final Set<RenderMode> MODES = RenderMode.asSet(
+        RenderMode.Component);
+
+    /**
+     * Creates a new component with its channel set to the given 
+     * channel.
+     * 
+     * @param componentChannel the channel that the component's 
+     * handlers listen on by default and that 
+     * {@link Manager#fire(Event, Channel...)} sends the event to 
+     */
+    public MoodlePrefsConlet(Channel componentChannel) {
+        super(componentChannel);
+    }
+
+    /**
+     * Trigger loading of resources when the console is ready.
+     *
+     * @param event the event
+     * @param consoleSession the console session
+     * @throws TemplateNotFoundException the template not found exception
+     * @throws MalformedTemplateNameException the malformed template name exception
+     * @throws ParseException the parse exception
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    @Handler
+    public void onConsoleReady(ConsoleReady event,
+            ConsoleSession consoleSession)
+            throws TemplateNotFoundException, MalformedTemplateNameException,
+            ParseException, IOException {
+        // Add resources to page
+        consoleSession.respond(
+            new AddConletType(type())
+                .addScript(new ScriptResource()
+                    .setScriptUri(event.renderSupport().conletResource(
+                        type(), "MoodlePrefs-functions.js"))
+                    .setScriptType("module"))
+                .addRenderMode(RenderMode.Component));
+    }
+
+    @Override
+    protected Set<RenderMode> doRenderConlet(RenderConletRequestBase<?> event,
+            ConsoleSession channel, String conletId,
+            Serializable conletState) throws Exception {
+        Set<RenderMode> renderedAs = new HashSet<>();
+        if (event.renderAs().contains(RenderMode.Component)) {
+            Template tpl
+                = freemarkerConfig().getTemplate("MoodlePrefs.ftl.html");
+            channel.respond(new RenderConlet(type(), conletId,
+                processTemplate(event, tpl,
+                    fmModel(event, channel, conletId, conletState)))
+                        .setRenderAs(RenderMode.Component)
+                        .setSupportedModes(MODES));
+            renderedAs.add(RenderMode.Component);
+        }
+        return renderedAs;
+    }
+
+}
